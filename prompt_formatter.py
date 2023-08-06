ee69ed4caac2253de8c662bc28e9aadf21caaf93
Stefan <shear.0-corned@icloud.com>
2023-08-06 09:10:35 +0300

    Optimize and rename Prompt formatter

--- prompt_formatter.py
+++ prompt_formatter.py
@@ -1,17 +1,14 @@
 def replace_special_characters(filename):
-    # Read the contents of the file
+    # Read the contents of the file and replace special characters
     with open(filename, 'r') as file:
-        content = file.read()
-
-    # Replace newline characters with \n and " with \"
-    content = content.replace('\n', '\\n').replace('"', '\\"')
+        content = file.read().replace('\n', '\\n').replace('\"', '\\\"')
 
     # Create a new file with the modified content
-    new_filename = filename.split('.')[0] + '_modified.txt'
+    new_filename = f"{filename.split('.')[0]}_modified.txt"
     with open(new_filename, 'w') as file:
         file.write(content)
 
-    print(f'Success! Modified content saved in {new_filename}')
+    print(f"Success! Modified content saved in {new_filename}")
 
 # Prompt the user for the filename
 filename = input("Enter the filename: ")
