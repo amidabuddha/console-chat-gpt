615ffd92ed61073d3ada7d73f5585c304cb8c852
MladenSU <mladen.s.uzunov@gmail.com>
2023-11-29 21:56:18 +0200

    Oops, missed the actual file ;)

---
+++ console_gpt/prompts/multiline_prompt.py
@@ -0,0 +1,37 @@
+from console_gpt.custom_stdin import custom_input
+from questionary import Style
+from typing import Union
+from console_gpt.catch_errors import eof_wrapper
+
+
+def _validate_description(val: str) -> Union[str, bool]:
+    """
+    Sub-function to _add_custom_role() which validates
+    the user input and does not allow empty values
+    :param val: The STDIN from the user
+    :return: Either error string or bool to confirm that
+    the user input is valid
+    """
+    if not val or val.startswith(" "):
+        return "Empty input not allowed!"
+    return True
+
+
+@eof_wrapper
+def multiline_prompt():
+    style = Style(
+        [
+            ("qmark", "fg:#86cdfc bold"),
+            ("question", "fg:#ffdb38 bold"),
+            ("answer", "fg:#69faff bold"),
+        ]
+    )
+    user_input = custom_input(
+        is_single_line=False,
+        message="Multiline input",
+        style=style,
+        qmark="❯",
+        validate=_validate_description,
+        multiline=True,
+    )
+    return {"role": "user", "content": user_input}
