ecc4d0706b14ed7c3ad28b7b0fc1e7a4a6a35c8d
Stefan <1759588+amidabuddha@users.noreply.github.com>
2024-01-09 08:58:03 +0200

    Remove helper file

--- delete_threads.py
+++
@@ -1,9 +0,0 @@
-import openai
-
-threads = ["thread_8UK3kdTZhiA4vhUthc33es84"]
-client = openai.OpenAI(api_key="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")
-
-for tread in threads:
-    response = client.beta.threads.delete(tread)
-    print(response)
-
