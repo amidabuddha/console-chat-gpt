9b7e9cc81a761832aa2245672b8d1b59d71627c7
Stefan <shear.0-corned@icloud.com>
2024-03-31 00:50:32 +0200

    [Improvement] Add command handler to ai managed chat

--- console_gpt/menus/ai_managed.py
+++ console_gpt/menus/ai_managed.py
@@ -6,6 +6,7 @@ import anthropic
 from console_gpt.config_manager import fetch_variable
 from console_gpt.custom_stdout import custom_print
 from console_gpt.menus.combined_menu import ChatObject
+from console_gpt.menus.command_handler import command_handler
 from console_gpt.menus.key_menu import set_api_key
 from console_gpt.prompts.temperature_prompt import temperature_prompt
 from console_gpt.prompts.user_prompt import chat_user_prompt
@@ -20,6 +21,8 @@ def managed_prompt() -> Tuple[ChatObject, str]:
     model_name, system_prompt, user_prompt = get_prompt(assistant)
     model_data = fetch_variable("models", model_name)
     model_data.update(dict(model_title=model_name))
+    if model_data["api_key"] in ("YOUR_OPENAI_API_KEY", "YOUR_MISTRALAI_API_KEY", "YOUR_ANTHROPIC_API_KEY"):
+        model_data = set_api_key(model_data)
     temperature = temperature_prompt()
     conversation = [{"role": "system", "content": system_prompt}]
     return ChatObject(model=model_data, conversation=conversation, temperature=temperature), user_prompt
@@ -38,7 +41,7 @@ def configure_assistant():
 
 def get_prompt(assistant):
     error_appeared = False
-    prompt = [chat_user_prompt()]
+    handled_prompt = command_catcher(assistant)
     client = anthropic.Anthropic(api_key=assistant["api_key"])
     try:
         response = client.messages.create(
@@ -46,7 +49,7 @@ def get_prompt(assistant):
             max_tokens=assistant["model_max_tokens"],
             temperature=0,
             system=assistant["role"],
-            messages=prompt,
+            messages=handled_prompt,
         ).model_dump_json()
     except anthropic.APIConnectionError as e:
         error_appeared = True
@@ -73,3 +76,18 @@ def get_prompt(assistant):
     response = json.loads(response)
     print(response["reasoning"])
     return response["prompt"]["model"], response["prompt"]["messages"][0]["content"], response["prompt"]["messages"][1]
+
+def command_catcher(assistant):
+    while True:
+        prompt = [chat_user_prompt()]
+        # Command Handler
+        handled_prompt = command_handler(assistant["model_title"], assistant["model_name"], prompt[0]["content"], prompt)
+        match handled_prompt:
+            case "continue" | None:
+                continue
+            case "break":
+                exit(1)
+            case _:
+                prompt[0]["content"] = handled_prompt
+                break
+    return prompt
