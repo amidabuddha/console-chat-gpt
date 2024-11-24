47eb6c6ee2954dff86fbcf229884dfd40488a1a1
Stefan <shear.0-corned@icloud.com>
2024-11-24 14:48:41 +0200

    Minor refactoring

--- console_gpt/menus/command_handler.py
+++ console_gpt/menus/command_handler.py
@@ -32,33 +32,33 @@ def command_handler(model_title, model_name, user_input, conversation, cached) -
             if not file_data:
                 return "continue"
             if not clarification:
-                if cached is not True:
-                    user_input = file_data
-                else:
+                if cached is True:
                     user_input = "This is the content of a file.", file_data
-            else:
-                if cached is not True:
-                    user_input = f"{clarification}:\n{file_data}"
                 else:
+                    user_input = file_data
+            else:
+                if cached is True:
                     user_input = clarification, file_data
+                else:
+                    user_input = f"{clarification}:\n{file_data}"
             return user_input
         case "format":
             clarification, multiline_data = multiline_prompt()
             if not multiline_data:
                 return "continue"
             if not clarification:
-                if cached is not True:
-                    user_input = multiline_data
-                else:
+                if cached is True:
                     user_input = (
                         "Please review the multiline input and perform the requested actions or answer.",
                         multiline_data,
                     )
-            else:
-                if cached is not True:
-                    user_input = f"{clarification}:\n{multiline_data}"
                 else:
+                    user_input = multiline_data
+            else:
+                if cached is True:
                     user_input = clarification, multiline_data
+                else:
+                    user_input = f"{clarification}:\n{multiline_data}"
             return user_input
         case "flush" | "new":
             # simply breaks this loop (inner) which start the outer one
@@ -75,15 +75,15 @@ def command_handler(model_title, model_name, user_input, conversation, cached) -
             if success:
                 clarification, webpage_data = additional_info(web_content)
                 if not clarification:
-                    if cached is not True:
-                        user_input = webpage_data
-                    else:
+                    if cached is True:
                         user_input = "This is the content of a webpage.", webpage_data
-                else:
-                    if cached is not True:
-                        user_input = f"{clarification}:\n{webpage_data}"
                     else:
+                        user_input = webpage_data
+                else:
+                    if cached is True:
                         user_input = clarification, webpage_data
+                    else:
+                        user_input = f"{clarification}:\n{webpage_data}"
                 return user_input
             return "continue"
         case "image":
@@ -103,7 +103,7 @@ def command_handler(model_title, model_name, user_input, conversation, cached) -
         case "exit" | "quit" | "bye":
             save_chat(conversation, ask=True)
         case _:
-            if cached is not True:
-                return user_input
-            else:
+            if cached is True:
                 return user_input, str(cached)
+            else:
+                return user_input
