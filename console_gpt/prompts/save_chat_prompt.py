amidabuddha/console-chat-gpt

commit 0c7f1af61f140c5e5df9ab82f223cc5dfa6f9602

[Bugfix] Skip asking to save chat if disabled in config

console_gpt/prompts/save_chat_prompt.py
@@ -38,6 +38,8 @@ def save_chat(conversation: List[Dict], ask: bool = False, skip_exit: bool = Fal
     if not _show_menu:
         if not skip_exit:
             custom_print("exit", "Goodbye, see you soon!", 130)
+        else:
+            return
 
     base_name = "chat"
     timestamp = datetime.now().strftime("%Y_%m_%d_%H%M%S")

commit ca1d8c0a3b6bfcf3dd30ab9dd639058aba504fc9

[BUGFIX] Re-Fix a bug introduced in 0c7f1af

console_gpt/prompts/save_chat_prompt.py
@@ -38,8 +38,6 @@ def save_chat(conversation: List[Dict], ask: bool = False, skip_exit: bool = Fal
     if not _show_menu:
         if not skip_exit:
             custom_print("exit", "Goodbye, see you soon!", 130)
-        else:
-            return
 
     base_name = "chat"
     timestamp = datetime.now().strftime("%Y_%m_%d_%H%M%S")