cc51eadadcc36f6a1903566faf20d23338fd34c7
MladenSU <mladen.s.uzunov@gmail.com>
2024-12-30 14:30:19 +0200

    [Improvement] Handle missing default model + optimizing the config calls

--- console_gpt/menus/model_menu.py
+++ console_gpt/menus/model_menu.py
@@ -20,15 +20,21 @@ def model_menu() -> Dict[str, Union[int, str, float]]:
 
     # Fetches the default model
     default_model = fetch_variable("defaults", "model")
+    all_models = fetch_variable("models")
+
+    if default_model not in all_models:
+        default_model = list(all_models.keys())[0] 
+        _show_menu = True
+
     if not _show_menu:
-        model_data = fetch_variable("models", default_model)
+        model_data = all_models[default_model]
         model_data.update(dict(model_title=default_model))
         return model_data
 
     # Build the menu based on the available models (chat.models.<model>)
-    menu_data = list(fetch_variable("models").keys())
+    menu_data = list(all_models.keys())
     menu_title = "{} Select a model:".format(use_emoji_maybe("\U0001F916"))
     selection = base_multiselect_menu("Model menu", menu_data, menu_title, default_model)
-    model_data = fetch_variable("models", selection)
+    model_data = all_models[selection]
     model_data.update(dict(model_title=selection))
     return model_data
