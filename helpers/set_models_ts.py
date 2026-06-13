7550abc6f77457c2b7cdc59aa7b864a3163dc5f7
Stefan <1759588+amidabuddha@users.noreply.github.com>
2026-06-13 10:34:56 +0300

    Add Type to TS MODELS_LIST constant

--- helpers/set_models_ts.py
+++ helpers/set_models_ts.py
@@ -3,5 +3,5 @@ import json
 from get_models import get_models
 
 models_list, models_max_token = get_models()
-print("export const MODELS_LIST = ", json.dumps(models_list, indent=4))
+print("export const MODELS_LIST: Record<string, string[]> = ", json.dumps(models_list, indent=4))
 print("export const MODELS_MAX_TOKEN: Record<string, number> = ", json.dumps(models_max_token, indent=4))
